be5d75cd7643d2076f39e0d962d10d002ebe39bb
Sneharaj7645653 <sneharaj7645653@gmail.com>
2026-02-22 23:47:08 +0530

    File name changed

--- SOLID/Ex6/src/Demo06.java
+++ SOLID/Ex6/src/Demo06.java
@@ -8,14 +8,23 @@ public class Main {
         NotificationSender email = new EmailSender(audit);
         NotificationSender sms = new SmsSender(audit);
         NotificationSender wa = new WhatsAppSender(audit);
+        NotificationSender pa = new WhatsAppSender(audit);
 
         email.send(n);
         sms.send(n);
+
         try {
             wa.send(n);
         } catch (RuntimeException ex) {
             System.out.println("WA ERROR: " + ex.getMessage());
             audit.add("WA failed");
+        }
+
+        try {
+            pa.send(n);
+        } catch (RuntimeException ex) {
+            System.out.println("PA ERROR: " + ex.getMessage());
+            audit.add("PA failed");
         }
 
         System.out.println("AUDIT entries=" + audit.size());
